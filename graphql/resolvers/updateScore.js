326309de02bc6eb8db24632d2ddfb71b46abc57d
Teddy <tedrandby@gmail.com>
2019-12-24 15:55:11 -0500

    got rid of the logging

--- graphql/resolvers/updateScore.js
+++ graphql/resolvers/updateScore.js
@@ -5,18 +5,14 @@ module.exports = {
         try {
             let data = {};
             const {id, teams} = await Tournament.findById(args._id);
-            console.log(teams);
             teams.forEach( team => {
-                console.log(team._id)
-                console.log(args.team_id)
+
                 if ( team._id == args.team_id ) {
                     team.score = args.score;
                     data = team;
-                    console.log(team);
                 }
             })
             await Tournament.findByIdAndUpdate(args._id, {teams: teams}, {useFindAndModify: false}) 
-            console.log(data);
             return data;
         } catch(err) {
             console.log(err);
