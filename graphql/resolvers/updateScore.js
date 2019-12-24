469f5cbce29eb27cc8e6af9349e22d7cee2f224a
Teddy <tedrandby@gmail.com>
2019-12-24 15:42:49 -0500

    fixed

--- graphql/resolvers/updateScore.js
+++ graphql/resolvers/updateScore.js
@@ -2,14 +2,16 @@ const Tournament = require("../models/tournament");
 
 module.exports = {
     updateScore: async args => {
-        try{
+        try {
+            let data = {};
             const {id, teams} = await Tournament.findById(args._id);
             teams.forEach( team => {
                 if ( team._id === args.team_id ) {
                     team.score = args.score;
+                    data = team;
                 }
             })
-            const data = await Tournament.findByIdAndUpdate(args._id, {teams: teams}) 
+            await Tournament.findByIdAndUpdate(args._id, {teams: teams}, {useFindAndModify: false}) 
             return data;
         } catch(err) {
             console.log(err);
