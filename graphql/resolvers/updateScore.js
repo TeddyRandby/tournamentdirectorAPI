2ff81a7a3b807045b84a8ec5e4b3e00e015566a1
Teddy <tedrandby@gmail.com>
2019-12-24 14:46:03 -0500

    added logging

--- graphql/resolvers/updateScore.js
+++ graphql/resolvers/updateScore.js
@@ -4,6 +4,7 @@ module.exports = {
     updateScore: async args => {
         try{
             const team = await Tournament.findByIdAndUpdate(args._id,{score: args.score})
+            console.log(team);
             return team;
         } catch(err) {
             console.log(err);
