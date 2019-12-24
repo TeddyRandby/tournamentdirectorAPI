24702b3a6f7729a5c2f2688df130472d0713621b
Teddy <tedrandby@gmail.com>
2019-12-24 12:23:07 -0500

    maybe got it

--- graphql/resolvers/createTournament.js
+++ graphql/resolvers/createTournament.js
@@ -3,6 +3,7 @@ const Tournament = require("../models/tournament");
 module.exports = {
   createTournament: async args => {
     try {
+      var newTournament;
       await Tournament.create(
         {
           teams: args.teams.teams
@@ -13,10 +14,11 @@ module.exports = {
             return handleError(err);
           } else {
             console.log(tournament);
-            return tournament;
+            newTournament = tournament;
           }
         }
       );
+      return newTournament;
     } catch (err) {
       console.log(err);
       throw err;
