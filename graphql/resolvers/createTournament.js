299ab17103a22ca5c2c1720a83e00eac8125b91b
Teddy <tedrandby@gmail.com>
2019-12-24 14:10:13 -0500

    agh

--- graphql/resolvers/createTournament.js
+++ graphql/resolvers/createTournament.js
@@ -3,7 +3,7 @@ const Tournament = require("../models/tournament");
 module.exports = {
   createTournament: async args => {
     try {
-      var teams = args.teams.reduce((obj, item) => (obj[item.name] = item, obj) ,{});
+      var teams = args.teams.teams.reduce((obj, item) => (obj[item.name] = item, obj) ,{});
       const data = await Tournament.create({
         teams: teams
       });
