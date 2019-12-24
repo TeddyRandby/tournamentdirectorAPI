21b5d069464bb43121c906033a7527a68f1937de
Teddy <tedrandby@gmail.com>
2019-12-24 15:15:20 -0500

    adding team id

--- graphql/resolvers/updateScore.js
+++ graphql/resolvers/updateScore.js
@@ -3,7 +3,7 @@ const Tournament = require("../models/tournament");
 module.exports = {
     updateScore: async args => {
         try{
-            const team = await Tournament.findByIdAndUpdate(args._id,{score: args.score}, {useFindAndModify: false})
+            const team = await Tournament.findOneAndUpdate({"_id": args._id, "teams._id": args.team_id}, {score: args.score})
             console.log(team);
             return team;
         } catch(err) {
