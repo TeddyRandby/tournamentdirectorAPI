761acc1214ba04a81ad34046da75cc4c95776fea
Teddy <tedrandby@gmail.com>
2019-12-24 15:36:07 -0500

    fixed

--- graphql/resolvers/updateScore.js
+++ graphql/resolvers/updateScore.js
@@ -3,10 +3,9 @@ const Tournament = require("../models/tournament");
 module.exports = {
     updateScore: async args => {
         try{
-            const [,teams] = await Tournament.findById(args._id);
-            
-            console.log(team);
-            return team;
+            const {id, teams} = await Tournament.findById(args._id);
+            console.log(teams);
+            return teams;
         } catch(err) {
             console.log(err);
         throw err;
