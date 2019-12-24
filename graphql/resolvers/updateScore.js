2a216de645487c7b144a4eaa0657326fb5a10492
Teddy <tedrandby@gmail.com>
2019-12-24 15:48:55 -0500

    even morel ogging

--- graphql/resolvers/updateScore.js
+++ graphql/resolvers/updateScore.js
@@ -5,7 +5,10 @@ module.exports = {
         try {
             let data = {};
             const {id, teams} = await Tournament.findById(args._id);
+            console.log(teams);
             teams.forEach( team => {
+                console.log(team._id)
+                console.log(args.team_id)
                 if ( team._id === args.team_id ) {
                     team.score = args.score;
                     data = team;
