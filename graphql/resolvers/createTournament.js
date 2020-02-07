da6eccf8aadd52767b791de0d4defdb568d01b20
Teddy <tedrandby@gmail.com>
2020-02-07 09:46:22 -0500

    can't create duplicate tournaments

--- graphql/resolvers/createTournament.js
+++ graphql/resolvers/createTournament.js
@@ -3,11 +3,15 @@ const Tournament = require("../models/tournament");
 module.exports = {
   createTournament: async args => {
     try {
+      if (!(await Tournament.findOne().where({name: args.name}))){
       const data = await Tournament.create({
         name: args.tournament.name,
         teams: args.tournament.teams
       });
       return data;
+    } else {
+      throw Error("Cannot have the same name as another tournament")
+    }
     } catch (err) {
       console.log(err);
       throw err;
