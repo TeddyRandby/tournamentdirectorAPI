89eaa28c7ce770328f2819feb29806a2b025815e
Teddy <tedrandby@gmail.com>
2020-11-10 21:08:26 -0500

    switched cors and express.json

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-const express = require('express');
-const graphqlHttp = require('express-graphql');
-const cors = require('cors');
-const graphqlSchema = require('./graphql/schema/index');
-const graphqlResolver = require('./graphql/resolvers/index');
-const mongoose = require('mongoose');
+const express = require("express");
+const graphqlHttp = require("express-graphql");
+const cors = require("cors");
+const graphqlSchema = require("./graphql/schema/index");
+const graphqlResolver = require("./graphql/resolvers/index");
+const mongoose = require("mongoose");
 const mongoURI = process.env.MONGODB_URI;
 const port = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-app.use('/api', graphqlHttp({
-  
+app.use(
+  "/api",
+  graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
-  
-  }));
-  
-  mongoose
-  .connect(
-    mongoURI, {
-      useNewUrlParser: true
-    }
-  ).then(() => {
-    app.listen(port, () => console.log(`Tournament Director API listening on port ${port}!`))
+    graphiql: true,
   })
-  .catch( err => {
+);
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Tournament Director API listening on port ${port}!`)
+    );
+  })
+  .catch((err) => {
     console.log(err);
   });
 
 //   mongoose.set("debug", (collectionName, method, query, doc) => {
 //     console.log(`${collectionName}.${method}`, JSON.stringify(query), doc);
 // });
+
